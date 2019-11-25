0ee779b2d351fbb39b72851b616af5c86d1e8031
lanamaxic <lana.maksic@gmail.com>
2019-11-25 16:11:14 +0100

    commit

---
+++ DOM/6-Removing_&_Replacing_Elements/app.js
@@ -0,0 +1,76 @@
+//REPLACE ELEMENTS
+
+//Create Element
+const newHeading = document.createElement('h2');
+
+// Add id
+newHeading.id = 'task-title';
+
+// New text node
+//let val = document.createTextNode('Easy Peasy')
+//val = document.querySelector('li').textContent
+//console.log(val);
+newHeading.appendChild(document.createTextNode('Task List')); //don't use innerText or textContent anymore, textNode is an object, textContent is a string
+
+// Get the old heading
+const oldHeading = document.getElementById('task-title');
+
+//Parent
+const cardAction = document.querySelector('.card-action');
+
+// Replace
+cardAction.replaceChild(newHeading, oldHeading);
+
+
+//EXERCISE REPLACE LI WITH A NEW ONE(replace text node)
+const listText = document.querySelector('.collection-item').firstChild;
+console.log(listText)
+let newText = document.createTextNode('New Item')
+let firstLi = document.querySelector('.collection-item')
+//firstLi.replaceChild(newText,listText)
+
+
+// REMOVE ELEMENT
+const lis = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// Remove list item
+lis[0].remove();
+
+// Remove child element
+list.removeChild(lis[3]);
+
+
+let listt = Array.from(document.querySelectorAll('li'));
+
+
+listt.forEach(function(item) {
+    console.log(item);
+    if (item.innerText == 'DONE')
+    item.remove();
+});
+
+
+/* CLASSES & ATTR
+const firstLi = document.querySelector('li:first-child');
+const link = firstLi.children[0];
+
+let val;
+
+// Classes
+val = link.className;
+val = link.classList;
+val = link.classList[0];
+link.classList.add('test');
+link.classList.remove('test');
+val = link;
+
+// Attributes
+val = link.getAttribute('href');
+val = link.setAttribute('href', 'http://google.com');
+link.setAttribute('title', 'Google');
+val = link.hasAttribute('title');
+link.removeAttribute('title');
+val = link;
+
+console.log(val);*/
---
+++ EXERCISE/Helveti_List/app.js
@@ -0,0 +1,22 @@
+const div = document.body.appendChild(document.createElement('div'));
+
+
+const h2 = document.body.appendChild(document.createElement('h2'));
+h2.appendChild(document.createTextNode('HelvetiList'));
+h2.style.fontFamily = 'Helvetica'
+
+const li1 = document.body.appendChild(document.createElement('li'));
+li1.appendChild(document.createTextNode('Zurich'));
+
+const li2 = document.body.appendChild(document.createElement('li'));
+li2.appendChild(document.createTextNode('Geneva'));
+
+const li3 = document.body.appendChild(document.createElement('li'));
+li3.appendChild(document.createTextNode('Winterthur'));
+
+const li4 = document.body.appendChild(document.createElement('li'));
+li4.appendChild(document.createTextNode('Lausanne'));
+
+const li5 = document.body.appendChild(document.createElement('li'));
+li5.appendChild(document.createTextNode('Lucerne'));
+
