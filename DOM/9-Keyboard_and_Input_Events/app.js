75490facc63972391af50314526fb596d669c226
lanamaxic <lana.maksic@gmail.com>
2019-11-28 09:49:41 +0100

    commit

---
+++ DOM/9-Keyboard_and_Input_Events/app.js
@@ -0,0 +1,46 @@
+const form = document.querySelector('form');
+const taskInput = document.getElementById('task');
+const heading = document.querySelector('h5');
+//const select = document.querySelector('select');
+
+// Clear input
+taskInput.value = '';
+
+// Submit
+//form.addEventListener('submit', runEvent)
+
+//Keydown
+//taskInput.addEventListener('keydown', runEvent)
+
+//Keypress
+//taskInput.addEventListener('keypress', runEvent)
+
+//Focus
+//taskInput.addEventListener('focus', runEvent)
+
+//Blur
+//taskInput.addEventListener('blur', runEvent)
+
+//Change
+//select.addEventListener('change', runEvent);
+
+// Event Handler
+
+function runEvent(e){
+   //e.preventDefault();
+   console.log(`Event Type: ${e.type} `);
+   heading.innerText = e.target.value;
+}
+
+////////////EXERCISE///////////////
+
+const select = document.querySelector('select');
+
+select.addEventListener('change', function (e) {
+    document.querySelector('#task-title').innerHTML = select.value
+    const li = document.querySelectorAll('li');
+    console.log(li[select.value-1]);
+    li[select.value-1].style.backgroundColor = 'red'; 
+    
+})
+
