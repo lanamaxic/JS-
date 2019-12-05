c30f4d4e4f4ecf3e0e1ee8ec7ece63ac7c8b4e9b
lanamaxic <lana.maksic@gmail.com>
2019-12-05 15:16:53 +0100

    commit

--- EXERCISE/Editable_Div/app.js
+++ EXERCISE/Editable_Div/app.js
@@ -1,30 +1,37 @@
-    let area = null;
+    let area = null;                               //empty object
     let view = document.getElementById('view');
 
-    view.onclick = function() {
-      editStart();
-    };
+
+    /*view.onclick = function() {
+        editStart();
+      };     OR */
+    
+        view.addEventListener('click', editStart)
 
     function editStart() {
         area = document.createElement('textarea');
         area.className = 'edit';
         area.value = view.innerHTML;
-  
-        area.onkeydown = function(event) {
-          if (event.key == 'Enter') {
-            this.blur();
-          }
-        };
-  
-        area.onblur = function() {
-          editEnd();
-        };
-  
+        area.addEventListener('keydown', function(event){
+            if (event.key == 'Enter') {
+                this.blur();
+              }
+
+        })
+        area.addEventListener('blur', function(){
+            editDone();
+        })
+
         view.replaceWith(area);
-        area.focus();
+        area.focus();                              //the focus should come at the end
+  
+        // area.onblur = function() {
+        //   editEnd();
+        // };
+   
       }
   
-      function editEnd() {
+      function editDone() {
         view.innerHTML = area.value;
         area.replaceWith(view);
       }
