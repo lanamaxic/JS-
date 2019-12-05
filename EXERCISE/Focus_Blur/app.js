99e4e63d2e4c57a20a30078f4a795227a411c9ef
lanamaxic <lana.maksic@gmail.com>
2019-12-05 09:31:07 +0100

    commit

---
+++ EXERCISE/Focus_Blur/app.js
@@ -0,0 +1,14 @@
+
+let focus = document.getElementById('task')
+focus.addEventListener('click', focusFuction);
+
+let blur = document.getElementById('task')
+blur.addEventListener('click', blurFunction);
+
+function focusFunction(){
+    document.getElementById("demo").innerHTML = "on Focus";
+}
+
+function blurFunction(){
+    document.getElementById("demo").innerHTML = "on Blur";
+}
