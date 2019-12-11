4fa1ef1366937fa27bd169d2ff99814edad844f3
lanamaxic <lana.maksic@gmail.com>
2019-12-11 09:10:00 +0100

    commit

--- DOM/11-Keyboard_Keydown&&KeyUp/app.js
+++ DOM/11-Keyboard_Keydown&&KeyUp/app.js
@@ -5,15 +5,18 @@ let lastTime = Date.now(); // we use it as a counter
 
 function handle(e) {
     console.log(keydown.checked)
-    if (form.elements[e.type + 'Ignore'].checked) return;//return is a break/ignore
+    //form.elements['keydownIgnore'].checked && form.elements['keyupIgnore'].checked
+    if (form.elements[e.type + 'Ignore'].checked) return//return is a break/ignore and it returns a value
 
     let text = e.type +
         ' key=' + e.key +
         ' code=' + e.code +
+        //e.shiftKey: it's a property of keyboard event returns true or false when shiftKey is pressed
         (e.shiftKey ? ' shiftKey' : '') +
         (e.ctrlKey ? ' ctrlKey' : '') +
         (e.altKey ? ' altKey' : '') +
         (e.metaKey ? ' metaKey' : '') +
+        //e.repeat: it's a property of keyboard event returns true or false when the key is holded down
         (e.repeat ? ' (repeat)' : '') +
         "\n";
 
@@ -21,10 +24,10 @@ function handle(e) {
         area.value += new Array(81).join('-') + '\n';
     }
     lastTime = Date.now();
+    //adding the text
+    area.value += text; //area.value = area.value + text
 
-    area.value += text;
-
-    if (form.elements[e.type + 'Stop'].checked) {
+    if (form.elements[e.type + 'Stop'].checked) { //property of the element(predefined)
         e.preventDefault();
     }
 }
